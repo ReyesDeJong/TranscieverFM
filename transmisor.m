b27fa3db39ad88dade005027a064fab5ba7e73f8
WINDOWS-MD4COD9\Felipe <felipe.bahamonde.m@ug.uchile.cl>
2016-11-24 23:01:33 -0300

    codigos ultimos

--- transmisor.m
+++ transmisor.m
@@ -29,61 +29,32 @@ triangular=cumsum(signal)/Fs;
 integradaplot(t,triangular);
 
 %señal modulada
-modulacion=B*cos(2*pi*fc*t+ (2*pi*fDev) * triangular);
-modulacion2=B*cos(2*pi*fc*t+ (2*pi*fDev2) * triangular);
-figure()
-subplot(2,1,1);
-plot(t,modulacion);
-subplot(2,1,2);
-plot(t,modulacion2);
+modulacion=B*cos(2*pi*fc*t+ (2*pi*fDev)/max(max(signal)) * triangular);
+modulacion2=B*cos(2*pi*fc*t+ (2*pi*fDev2)/max(max(signal)) * triangular);
+modulaciont(t,modulacion,modulacion2);
 
 %furier de modulacion
 fourierm=fft(modulacion);
-P2m = abs(modulacion/L);
+P2m = abs(fourierm/L);
 P1m = P2m(1:L/2+1);
 P1m(2:end-1) = 2*P1m(2:end-1);
 fourierm2=fft(modulacion2);
-P2m2 = abs(modulacion2/L);
+P2m2 = abs(fourierm2/L);
 P1m2 = P2m2(1:L/2+1);
 P1m2(2:end-1) = 2*P1m2(2:end-1);
-figure()
-subplot(2,1,1);
-plot(f,P1m);
-subplot(2,1,2);
-plot(f,P1m2);
-
+modulacionfplot(f,P1m,P1m2);
                                                         %% parte tres
 ruido = awgn(modulacion,15);
 ruido2 = awgn(modulacion,30);
-figure();
-subplot(2,1,1);
-plot(t,ruido);
-subplot(2,1,2);
-plot(t,ruido2);
-figure()
+modulacionruido(t,ruido,ruido2);
+
 ruido3 = awgn(modulacion2,15);
 ruido4 = awgn(modulacion2,30);
-subplot(2,1,1);
-plot(t,ruido3);
-subplot(2,1,2);
-plot(t,ruido4);
+modulacionruido(t,ruido3,ruido4);
 
                                                         %% parte cuatro
-len = size(ruido,1);
-if(len==1)
-    ruido = ruido(:);
-end
-
-
-t = (0:1/Fs:((size(ruido,1)-1)/Fs))';
-t = t(:,ones(1,size(ruido,2)));
 
 yq = hilbert(ruido).*exp(-j*2*pi*fc*t);
-z = (1/(2*pi*fDev))*[zeros(1,size(yq,2)); diff(unwrap(angle(yq)))*Fs];
-
-% --- restore the output signal to the original orientation --- %
-if(len == 1)
-    z = z';
-end
+z = (1/(2*pi*fDev))*[zeros(1,size(yq',2)); diff(unwrap(angle(yq')))*Fs];
 figure();
 plot(t,z);
